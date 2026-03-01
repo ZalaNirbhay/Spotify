7b321d40a40ba15a80dd497250db2e17fb1657a0
zalanirbhay21@gmail.com <zalanirbhay21@gmail.com>
2026-03-01 20:24:17 +0530

    connecting front-end with backend and allowing the insert,update,delete and view request using cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
+    app.use(express.json());
+    app.use(cookieParser());
+    app.use(cors());
+        
+app.use((req, res, nxt) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  nxt();
+});
+
+
+    
+
+
+
 
 module.exports = app;
