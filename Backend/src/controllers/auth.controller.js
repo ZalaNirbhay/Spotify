80398315915e7225aa02c87cbf64c33721a4bee0
zalanirbhay21@gmail.com <zalanirbhay21@gmail.com>
2026-03-08 17:38:07 +0530

    login api for user is created for user log in using email or username

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -42,11 +42,54 @@ async function registerUser(req, res) {
   });
 }
 
+async function loginUser(req, res) {
+  const { username, email, password } = req.body;
 
+  if (!password) {
+    return res.status(400).json({ message: "Password is required" });
+  }
 
+  const user = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
 
+  if (!user) {
+    return res.status(401).json({
+      message: "Invalid credentials",
+    });
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordCorrect) {
+    return res.status(401).json({
+      message: "Invalid credentials",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "User logged in successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    },
+    token,
+  });
+}
 
 module.exports = {
   registerUser,
-
+  loginUser,
 };
